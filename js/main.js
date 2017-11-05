ded00264ab5a706249d99bf0dd1f2e9b17af5f62
Ridwan Sanusi <ridwan.sanusi@booking.com>
2017-11-05 23:21:20 +0100

    Updated the list object to add to the storedquestions object

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-// ToDo 
+// ToDo
 // - Get value from textarea and add it to the array
 
 var questionBtn = document.querySelector('.questionBtn');
 var questionField = document.querySelector('.questionInput');
 var questionBlock = document.querySelector('.quizBlock');
+var questionContainer = document.querySelector('.questionWrap');
+var listTemplate = questionContainer.querySelector('.questionCard.template');
 var storedQuestions;
 
 
@@ -17,29 +19,22 @@ function main() {
 	}
 }
 
-
-main();
-
-
 function save() {
 	localStorage["questionList"] = JSON.stringify(storedQuestions);
 }
 
 function load() {
-	if(!localStorage["questionList"]) {
-		return false;
-	} else {
+	if(!localStorage["questionList"])
+	return false;
+
 	// retrive Array
-		storedQuestions = JSON.parse(localStorage["questionList"]);
-	return true;
-	}
+	storedQuestions = JSON.parse(localStorage["questionList"]);
+		return true;
 }
 
 
-
-
-
 questionBtn.addEventListener('click', function onEvent(event){
+
 	// prevent defualt behaviour
 	event.preventDefault();
 
@@ -47,26 +42,22 @@ questionBtn.addEventListener('click', function onEvent(event){
 		questionField.innerHTML = questionField.value;
 
 		// Save it to the quetionField Array
-
 		storedQuestions.push({
-			question: questionField.value,
-			answerA: "",
-			answerB: "",
-			answerC: "",
-			answerD: ""
+			question: questionField.value
 		});
 
 		// update the Array in localstorage
 		save();
-		
-		//TODO: Feedback to be added
-		console.log("Saved the value to the array");
+
+		//Reload and display Updated Questions
+		questionContainer.innerHTML = '';
+		displayQuestions();
+
+
 
 		//Remove the value
 		questionField.value = "";
 
-		//Reload and display Updated Questions
-		displayQuestions();
 
 	} else {
 		//TODO: Feedback to be added
@@ -76,12 +67,11 @@ questionBtn.addEventListener('click', function onEvent(event){
 
 });
 
-function displayQuestions() {
-	var questionContainer = document.querySelector('.questionWrap');
-	var listTemplate = questionContainer.querySelector('.questionCard.template');
 
-	storedQuestions.forEach(function(question, i) { 
-		
+
+function displayQuestions() {
+
+	storedQuestions.forEach(function(question, i) {
 
 		// Clone the questionCard and append it in the "ul"
 		var questionCard = listTemplate.cloneNode(true);
@@ -89,9 +79,64 @@ function displayQuestions() {
 		questionContainer.appendChild(questionCard);
 
 		//Bind the each questions in the array to their respective questioncard
-
-		var questionText = questionCard.querySelector('.questionText');
+		var questionText = questionCard.querySelector('.question');
 		questionText.innerHTML = question.question;
 
-	});
+
+		// Answer Button should open answer form
+		var answerForm = questionCard.querySelector('.answerForm');
+		var answerFormBtn = questionCard.querySelector('.answerBtn');
+
+		// Initially hide the class until the button is clicked
+		answerForm.classList.add('hide');
+
+		// On the button click, reveal the answer panel
+		answerFormBtn.addEventListener("click", function(){
+			event.preventDefault();
+			answerForm.classList.remove('hide');
+		});
+
+		// Receive new answer and add it to the list of answers
+		var answerForm = questionCard.querySelector('.answerForm');
+		var answerInput = answerForm.querySelector('.answerInput');
+		var answerInputBtn = answerForm.querySelector('.priBtn');
+
+		// when the answerBtn is clicked add the input to the array
+		answerInputBtn.addEventListener("click", function(){
+			event.preventDefault();
+
+			if (answerInput && answerInput.value) {
+
+				storedAnswers = [];
+
+				// Save it to the storedAnswers Array
+				storedAnswers.push({
+					text: answerInput.value,
+					correct: false
+				});
+
+				// Added the storedAnswers object inside storedQuestions
+				storedQuestions[i].answers = storedAnswers;
+
+				// update the Array in localstorage
+				save();
+
+				// Now display it on the page
+				var answerList = questionCard.querySelector('.answerList');
+				var answerCard = answerList.querySelector('.answerCard');
+
+				console.log(answerList);
+				// Show this on the page!
+				console.log(answerInput.value);
+
+
+			} else {
+				console.log('Theres no input');
+			}
+
+		});
+
+	}); //End of the forEach Loop
 }
+
+main();
--- js/playground.js
+++ js/playground.js
@@ -9,24 +9,39 @@ var family = [
 	{name: 'zahra', role: 'daughter'}
 ];
 
-family.forEach(function(name, i) { 
+var extFamily = [
+	{name: 'shakiti', role: 'stepfather'}
+];
+
+
+var storedQuestions = [{question: 'who is the president of the USA?'}];
+
+
+var storedAnswers =  [
+	{ text: "Obama", isCorrect: false },
+	{ text: "Trump", isCorrect: true },
+	{ text: "babangida", isCorrect: false },
+	{ text: "Netanyahu", isCorrect: false },
+]
+
+storedQuestions.push(storedAnswers);
+
+
+family.forEach(function(name, i) {
 	var listCard = template.cloneNode(true);
 	listCard.classList.remove('template');
 	listContainer.appendChild(listCard);
-	
+
 	var title = listCard.querySelector('.card-title');
 	var title = listCard.querySelector('.card-title');
 
-	
+
 	title.innerText = name.name;
 
 	//Remove the list template
 
-	console.log(name.name);
+	//console.log(name.name);
 });
 
 
 //console.log(listTemplate);
-
-
-
